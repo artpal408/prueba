567da51dcf76776cdeb500b02e6bb2de0c088912
Lab2Ies <artpal2014@gmail.com>
2017-09-03 16:28:30 -0600

    prueba

--- noticias.php
+++ noticias.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $titulo = $_POST['titulo'];
 $contenido = $_POST['contenido'];
 
@@ -8,5 +7,29 @@ require_once 'driver.php';
 
 $enlace = new DMysqli();
 
+//$estado = $enlace->query("insert into noticias VALUES(DEFAULT, '" . $titulo . "','" . $contenido ."') ");
+//
+//if ($estado == true){
+//    echo 'Exito agregando noticia';
+//}
+//else {
+//    echo 'Fallo al agregar noticia';
+//}
 
 
+
+$exito = $enlace->query("delete from noticias where id=2");
+if ($exito == true){
+    echo 'Exito eliminando noticias';
+}
+else {
+    echo 'Fallo al eliminar noticias';
+}
+
+$noticias = $enlace->multiples_datos("SELECT * FROM noticias");
+
+
+$noticias = $enlace->datos("select * from noticias where id = 1");
+
+
+?>
