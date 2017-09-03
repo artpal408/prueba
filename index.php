f1e2c04db0dc89612daaa697cb485c6124a86d24
Lab2Ies <artpal2014@gmail.com>
2017-09-03 16:06:04 -0600

    prueba

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<form action="noticias.php" method="post">
+
+
+    <input placeholder="Escriba titulo" name="titulo" type="text">
+    <br>
+    <input placeholder="Escriba contenido" name="contenido" type="text">
+    <br>
+
+    <input value="Enviar" type="submit">
+</form>
---
+++ noticias.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$titulo = $_POST['titulo'];
+$contenido = $_POST['contenido'];
+
+require_once 'driver.php';
+
+$enlace = new DMysqli();
+
+
+
